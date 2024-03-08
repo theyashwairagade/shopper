5fd2e3601632167b976082cbab770ae44c7cd53d
Yash Wairagade <theyashwairagade@gmail.com>
2024-03-08 13:18:13 +0530

    Add /upload route for handling file uploads

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,25 @@ app.get("/", (req, res) => {
   res.send("Express app is running");
 });
 
+// Image storage engine
+const storage=multer.diskStorage({
+  destination:'./upload/images',
+  filename:(req,file,cb)=>{
+    return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+  }
+})
+
+const upload=multer({storage:storage})
+
+// Creating upload endpoint for images
+app.use('/images',express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`
+  });
+});
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on port " + port);

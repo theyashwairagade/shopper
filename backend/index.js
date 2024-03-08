7cc5cf89f69f50a437895f71b0fc3708208fc523
Yash Wairagade <theyashwairagade@gmail.com>
2024-03-08 14:11:56 +0530

    Add POST /addproduct route

--- backend/index.js
+++ backend/index.js
@@ -40,6 +40,69 @@ app.post("/upload", upload.single('product'), (req, res) => {
   });
 });
 
+//  Schema for Creating Products
+const Product= mongoose.model("Product",{
+  id:{
+    type:Number,
+    required:true
+  },
+  name:{
+    type:String,
+    required:true
+  },
+  image:{
+    type:String,
+    required:true
+  },
+  category:{
+    type:String,
+    required:true
+  },
+  new_price:{
+    type:Number,
+    required:true
+  },
+  old_price:{
+    type:Number,
+    required:true
+  },
+  date:{
+    type:Date,
+    default:Date.now
+  },
+  available:{
+    type:Boolean,
+    default:true
+  }
+})
+
+app.post('/addproduct',async(req,res)=>{
+  let products=await Product.find({});
+  let id;
+  if(products.length>0){
+    let last_product_array=products.slice(-1);
+    let last_product=last_product_array[0];
+    id=last_product.id+1;
+  }else{
+    id=1;
+  }
+  const product=new Product({
+    id:id,
+    name:req.body.name,
+    image:req.body.image,
+    category:req.body.category,
+    new_price:req.body.new_price,
+    old_price:req.body.old_price
+  });
+  console.log(product);
+  await product.save();
+  console.log("Saved");
+  res.json({
+    success:true,
+    name:req.body.name
+  })
+})
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on port " + port);

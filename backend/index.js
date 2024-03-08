be8987bf155f2db153c97a8620ce73363a81c3a2
Yash Wairagade <theyashwairagade@gmail.com>
2024-03-08 14:31:20 +0530

    Add Get /allproducts route

--- backend/index.js
+++ backend/index.js
@@ -113,6 +113,13 @@ app.post('/removeproduct',async(req,res)=>{
   })
 })
 
+// Creating Route for getting all products
+app.get('/allproducts',async(req,res)=>{
+  let products= await Product.find({});
+  console.log("All products fetched");
+  res.send(products);
+})
+
 app.listen(port, (error) => {
   if (!error) {
     console.log("Server running on port " + port);
